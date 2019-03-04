0cf8a551d8b28860daccdc9c8f02f6bc3607ca6c
MartMilani <martino.milani94@gmail.com>
2019-03-04 14:06:34 +0100

    corrected bugs in optimization.py

--- optimization.py
+++ optimization.py
@@ -19,12 +19,11 @@ def normalized_laplacian(W):
 def buildW(flat_w_sup, N):
     W = np.zeros((N, N))
     shift = 0
-    for i in range(N):
-        W[i, i:] = flat_w_sup[shift:shift+N-i]
-        shift += N-i
+    for i in range(N-1):
+        row_length = N-i-1
+        W[i, i+1:] = flat_w_sup[shift:shift+row_length]
+        shift += row_length
     W = W+W.T
-    for i in range(N):
-        W[i, i] /= 2
     return W
 
 
@@ -63,38 +62,69 @@ def harmonics(N):
 
 
 def main():
+
+    # number of vertices on the ring graph
     N = 4
+
+    # building the pointset
     thetas = np.linspace(0, 2 * np.pi, N, endpoint=False)
     assert len(thetas) == N, len(thetas)
     pointset = np.zeros((N, 2))
     pointset[:, 0] = np.cos(thetas)
     pointset[:, 1] = np.sin(thetas)
-    vect = spatial.distance.cdist(pointset[0].reshape((1, 2)), pointset)
+
+    # buiding the initial graph
+    vect = spatial.distance.cdist(pointset[0].reshape((1, 2)), pointset)[0]
     weights = np.exp(-vect / np.mean(vect))
+    weights[0] = 0
     W0 = circulant(weights)
-    print(W0)
+    print("initial weight vector:\n", weights)
+    print("Initial adjacency matrix:\n", W0)
+
+    # calculating the circle armonics of the circle
     U = harmonics(N)
+
+    # storing the upper triangular adjacency matrix in a flat vector
     w = np.ndarray(0)
-    for i in range(np.alen(W0)):
-        w = np.append(w, W0[i, i:])
-    assert len(w) == N*(N+1)/2
-    lambdas = np.ones(N)
+    for i in range(np.alen(W0)-1):
+        w = np.append(w, W0[i, i+1:])
+    assert len(w) == N*(N-1)/2
+
+    # initializing the lambdas
+    lambdas = np.ones(N)/N
+
+    # initializing the vector of w, lambdas
     X0 = np.concatenate([w, lambdas])
+
+    # defining the bounds on the variables w, lambda
     lb = np.zeros(len(w) + N)
     ub = np.ones(len(w) + N) * np.inf
     bounds = scipy.optimize.Bounds(lb=lb, ub=ub)
+
+    # defining the constraint on the lambdas
     A = np.zeros(len(w) + N)
     A[len(w):] = 1
     b = 1
     cons = [{"type": "eq", "fun": lambda x: A @ x - b}]  # Ax == b
+
+
+
+    # performing the minimization step
     result = minimize(loss_function, X0, args=(U, N), bounds=bounds,
                       constraints=cons)
+
+    # retrieving the results
     Wsup_and_lambda = result.x
+
     flat_w_sup = Wsup_and_lambda[:-N]
     W = buildW(flat_w_sup, N)
     lambda_ = Wsup_and_lambda[-N:]
-    print(W)
-    print(lambda_)
+
+    # printing the results
+    print("Final adjacency matrix:\n", W)
+    print("Final lambdas:\n", lambda_)
+    print("Initial Loss = ", loss_function(X0, U, N))
+    print("Final Loss = ", loss_function(Wsup_and_lambda, U, N))
 
 
 if __name__ == '__main__':
