4b1db0493b2f2abdf2e991ee5978905594bb87cd
MartMilani <martino.milani94@gmail.com>
2019-03-07 12:14:44 +0100

    fix bug in 04_optimization.py

--- 04_optimization.py
+++ 04_optimization.py
@@ -2,7 +2,6 @@ import scipy
 import numpy as np
 from scipy.optimize import minimize
 from scipy import spatial
-from scipy.linalg import circulant
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -35,56 +34,56 @@ def O_LU(W, U):
     return L@U
 
 
-def O_Lambda(lambda_, U):
-    return np.diag(lambda_)@U
+def O_LambdaU(lambda_, U):
+    return U @ np.diag(lambda_)
 
 
 def loss_function(Wsup_and_lambda, U, N):
     flat_w_sup = Wsup_and_lambda[:-N]
     W = buildW(flat_w_sup, N)
     lambda_ = Wsup_and_lambda[-N:]
-    return np.linalg.norm(O_LU(W, U) - O_Lambda(lambda_, U), ord='fro')
+    return np.linalg.norm(O_LU(W, U) - O_LambdaU(lambda_, U), ord='fro')
 
 
-def harmonics(N):
-    sigma = 0.002
-    thetas = np.linspace(0, 2 * np.pi, N, endpoint=False)
-    thetas += sigma * np.random.randn(N)
+def harmonics(thetas):
+    N = len(thetas)
+    assert N % 2 == 1, 'N has to be odd to construct the last eigenvector'
     U = np.zeros((N, N))
     k = 0
     for i in range(N):
         if i == 0:
-            U[:, i] = 0
-        if i % 2 == 1:
+            U[:, 0] = 1. / (np.sqrt(2)*np.pi)
+        elif i % 2:
             k += 1
-            U[:, i] = np.sin(k * thetas) / np.sqrt(np.pi)
-        if i % 2 == 0:
-            U[:, i] = np.cos(k * thetas) / np.sqrt(np.pi)
+            U[:, i] = np.sin(k * thetas) / (np.pi)
+        else:
+            U[:, i] = np.cos(k * thetas) / (np.pi)
     return U
 
 
 def main():
 
     # number of vertices on the ring graph
-    N = 14
+    # WARNING: it has to be an odd number for the construction of the correct harmonics
+    N = 21
 
     # building the pointset
     thetas = np.linspace(0, 2 * np.pi, N, endpoint=False)
+    # thetas += np.random.rand(N)/N*2*np.pi
     assert len(thetas) == N, len(thetas)
     pointset = np.zeros((N, 2))
     pointset[:, 0] = np.cos(thetas)
     pointset[:, 1] = np.sin(thetas)
 
     # buiding the initial graph
-    vect = spatial.distance.cdist(pointset[0].reshape((1, 2)), pointset)[0]
-    weights = np.exp(-vect / np.mean(vect))
-    weights[0] = 0
-    W0 = circulant(weights)
-    print("initial weight vector:\n", weights)
+    distance_matrix = spatial.distance.cdist(pointset, pointset)
+    W0 = np.exp(-distance_matrix / np.mean(distance_matrix))
+    for i in range(np.alen(W0)):
+        W0[i, i] = 0
     print("Initial adjacency matrix:\n", W0)
 
     # calculating the circle armonics of the circle
-    U = harmonics(N)
+    U = harmonics(thetas)
 
     # storing the upper triangular adjacency matrix in a flat vector
     w = np.ndarray(0)
@@ -125,9 +124,33 @@ def main():
     print("Final lambdas:\n", lambda_)
     print("Initial Loss = ", loss_function(X0, U, N))
     print("Final Loss = ", loss_function(Wsup_and_lambda, U, N))
+
     sns.heatmap(W0)
+    plt.title('initial weights')
     plt.show()
     sns.heatmap(W)
+    plt.title('optimal weights')
+    plt.show()
+
+    eigvalues0, eigenvectors0 = np.linalg.eig(laplacian(W0))
+    eigvalues, eigenvectors = np.linalg.eig(laplacian(W))
+    eigenvectors = eigenvectors[:, np.argsort(eigvalues)]
+    eigenvectors0 = eigenvectors0[:, np.argsort(eigvalues0)]
+
+    sns.heatmap(abs(eigenvectors0.T @ U))
+    plt.title('harmonics @ initial U')
+    plt.show()
+
+    sns.heatmap(abs(eigenvectors.T @ U))
+    plt.title('harmonics @ final U')
+    plt.show()
+
+    print("||initial eigenvectors @ harmonics.T|| = ", np.linalg.norm(eigenvectors0 @ (U.T)))
+    print("||final eigenvectors @ harmonics.T|| = ", np.linalg.norm(eigenvectors @ (U.T)))
+
+    plt.plot(eigenvectors0[:, :3])
+    plt.show()
+    plt.plot(eigenvectors[:, :3])
     plt.show()
 
 
