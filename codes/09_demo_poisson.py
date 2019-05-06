aba1538b5cc16ae2ee411ddc26e20f135f399c97
MartMilani <martino.milani94@gmail.com>
2019-05-06 18:13:51 +0200

    comparison FEM and graph diffusion

--- codes/09_demo_poisson.py
+++ codes/09_demo_poisson.py
@@ -11,8 +11,9 @@ from dolfin import *
 import scipy
 from scipy.sparse import csr_matrix
 
-for nside in [32, 64, 128]:
+for nside in [1, 2, 4]:
     # Create mesh and define function space
+    # BIG PROBLEM: I HAVE NO IDEA OF HOW THE PIXELS ARE ORDERED!
     meshname = "09_meshes/HEALPix_{}.xml".format(nside)
     mesh = Mesh(meshname)
     global_normal = Expression(("x[0]", "x[1]", "x[2]"), degree=1)
@@ -22,20 +23,25 @@ for nside in [32, 64, 128]:
     # Define variational problem
     u = TrialFunction(V)
     v = TestFunction(V)
-    f = Expression("x[0]", degree=4)
-
-    a = inner(grad(u), grad(v))*dx
-    L = f*v*dx
+    f = Expression("0", degree=1)
+    u_n = interpolate(Expression("pow(x[0], 2)", degree=1), V)
+    dt = 100
+    a =  u*v*dx + dt*dot(grad(u), grad(v))*dx
+    L = (u_n + dt*f)*v*dx
 
     # Compute solution and save stiffness matrix!
+
     u = Function(V)
+   
+
+    solve(a == L, u)
+    
+
+    # Save solution in pvd format
+    file = File("poisson_{}.pvd".format(nside))
+    file << u
+    
     A = assemble(a)
     A_mat = as_backend_type(A).mat()
     A_sparray = csr_matrix(A_mat.getValuesCSR()[::-1], shape=A_mat.size)
     scipy.sparse.save_npz('stiffnessmatrix_{}.npz'.format(nside), A_sparray)
-
-    # solve(a == L, u)
-
-    # Save solution in pvd format
-    # file = File("poisson_{}.pvd".format(nside))
-    # file << u
