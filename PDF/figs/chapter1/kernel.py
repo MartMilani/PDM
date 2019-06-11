06b9518e26e44b89480e5122a7315250b4b1957d
MartMilani <martino.milani94@gmail.com>
2019-06-11 10:47:35 +0200

    first Chapter drat

--- PDF/figs/chapter1/kernel.py
+++ PDF/figs/chapter1/kernel.py
@@ -7,9 +7,12 @@ def kernel(x, t):
 
 
 xx = np.linspace(0, 2, 100)
-t = 0.1
+t = 0.5
 plt.plot(xx, kernel(xx, t))
 for x in np.linspace(0, 2, 11):
     if x:
-        plt.plot([x, x], [0, kernel(x, t)], 'r--')
+        """if x<0.3:
+            plt.plot([x, x], [0, kernel(x, t)], 'r--')"""
+
+        plt.plot([x, x], [0, kernel(x, t)], 'b--')
 plt.show()
